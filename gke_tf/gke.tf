852e878f71dd9ff7923806d660a5cdc015840ae7
Yogitatest <97597212+Yogitatest@users.noreply.github.com>
2024-06-25 21:35:12 +0200

    Update gke.tf

--- gke_tf/gke.tf
+++ gke_tf/gke.tf
@@ -31,7 +31,7 @@ resource "google_container_node_pool" "demo_nodes" {
     oauth_scopes = [
       
       "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring"
+      "https://www.googleapis.com/auth/monitoring",
     ]
   }
 }
