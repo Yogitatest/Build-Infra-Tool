cd83f5f8dfb161feed16fbc884b3f3bcd40bd399
Yogitatest <97597212+Yogitatest@users.noreply.github.com>
2024-06-25 21:49:53 +0200

    Create provider.tf

---
+++ gke_tf/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+
+  required_version = ">= 0.12.26"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.43.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.43.0"
+    }
+  }
+}
